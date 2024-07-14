4b72031cac027249ac39ad7a1927ef29a3477b77
Simon Wandel <simon.wandel@live.de>
2024-07-14 07:57:03 +0200

    #2 initial commit

---
+++ src/main/java/com/oslo/snacksnuggles/SnacksnugglesApplication.java
@@ -0,0 +1,13 @@
+package com.oslo.snacksnuggles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnacksnugglesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnacksnugglesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oslo/snacksnuggles/SnacksnugglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslo.snacksnuggles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnacksnugglesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
